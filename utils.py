85d579c74f58676848cbea648bfb7769f8f6bfc6
Riccardo Metere <metere@cbs.mpg.de>
2017-02-23 20:18:05 +0100

    N/A

---
+++ utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+quarkball2017
+"""
+
+# ======================================================================
+# :: Future Imports
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals)
+
+
+import numpy as np
+
+# ======================================================================
+# :: data structures example
+istance = dict(
+    videos=np.array(num_videos, dtype=int),
+    num_endpoints=int,
+    requests=list((video, endpoint, num)),
+    num_caches=int,
+    cache_size=int,
+    latencies=np.array(dtype=int).shape((num_endpoints, num_caches))
+)
+
+caching= dict(
+    used=int,
+    content=list(),
+)
+
+
+# ======================================================================
+def validate(caching, videos):
+    """
+    Validate a caching.
+
+    Args:
+        caching (dict):
+        videos (dict):
+
+    Returns:
+
+    """
+    return validation
+
+
+# ======================================================================
+def score(caching, requests):
+    """
+
+    Args:
+        caching ():
+
+    Returns:
+
+    """
+    return scoring
